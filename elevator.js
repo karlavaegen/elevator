994f3ccaf1aaebc6f5a7c0e98d7a0e1d3b72bb2a
karlavaegen <david@karlavaegen.com>
2016-05-28 19:12:57 -0700

    wip

--- elevator.js
+++ elevator.js
@@ -1,48 +1,64 @@
 {
     init: function(elevators, floors) {
-        var globalQueue = [];
 
-        function getClosestFloor(ary, currentFloor){
-            ary.sort(function(a,b){
-                if(Math.abs(b - currentFloor) < Math.abs(a - currentFloor)){
-                    return -1;
+        for(var i = 0; i < elevators.length; i++){
+            var elevator = elevators[i];
+
+            elevator.smartQueue = function(floorNum){
+                var queue = elevator.destinationQueue;
+                if(queue.indexOf(floorNum) === -1){
+                    queue.push(floorNum);
                 }
-                else if(Math.abs(b - currentFloor) > Math.abs(a - currentFloor)){
-                    return 1;
+                queue = queue.filter(function(value, index, self){
+                    return self.indexOf(value) === index;
+                });
+
+                if(elevator.destinationDirection() === "stopped"){
+                    queue = queue.filter(function(value){
+                        return value != elevator.currentFloor();
+                    });
+                }
+                else if(elevator.destinationDirection() === "up"){
+                    queue.sort();
+                    for(var i = 0; i < queue; i++){
+                        if(queue[i] < elevator.currentFloor()){
+                            queue.push(queue.splice(i, 1));
+                        }
+                    }
                 }
                 else {
-                    return 0;
+                    queue.sort(function(a,b){
+                        return b - a;
+                    });
+                    for(var i = 0; i < queue; i++){
+                        if(queue[i] > elevator.currentFloor()){
+                            queue.push(queue.splice(i, 1));
+                        }
+                    }
                 }
-            });
-            return ary.shift();
-        };
 
-        for(var i = 0; i < elevators.length; i++){
-            var elevator = elevators[i];
-            elevator.on("idle", function(){
-                if(globalQueue.length > 0){
-                    var dest = getClosestFloor(globalQueue, elevator.currentFloor());
-                    elevator.goToFloor(dest);
-                }
-            });
-        }
+                console.log(elevator.destinationQueue);
+                elevator.checkDestinationQueue();
+            }
 
-        for(var i = 0; i < floors.length; i++){
-            var floor = floors[i];
-            floor.on("up_button_pressed", function(){
-                var floorNum = floor.floorNum();
-                globalQueue.unshift(floorNum);
-            });
-            floor.on("down_button_pressed", function(){
-                var floorNum = floor.floorNum();
-                globalQueue.unshift(floorNum);
-            });
-        }
 
-        for(var i = 0; i < elevators.length; i++){
-            var elevator = elevators[i];
+            for(var i = 0; i < floors.length; i++){
+                var floor = floors[i];
+                floor.on("up_button_pressed", function(){
+                    elevator.smartQueue(floor.floorNum());
+                });
+                floor.on("down_button_pressed", function(){
+                    elevator.smartQueue(floor.floorNum());
+                });
+            }
+
             elevator.on("floor_button_pressed", function(floorNum){
-                globalQueue.unshift(floorNum);
+                elevator.smartQueue(floorNum);
+            });
+
+            elevator.on("idle", function(){
+                console.log("was stuck for some reason");
+                elevator.goToFloor(0);
             });
         }
     },
